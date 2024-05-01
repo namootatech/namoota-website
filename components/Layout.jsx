ce82fb66f5d557b3b45618c76c8f5ce4e7b0cf84
zwelisha <zwescientist@gmail.com>
2024-05-01 13:13:42 +0200

    Added SEO keywords and fixed react syntax

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -11,6 +11,10 @@ const Layout = ({ children }) => {
           name='description'
           content='Namoota making it simple for you to build and grow your SaaS applications, or any business idea'
         />
+        <meta name="keywords" content="www.namoota.co.za,software development company, best software development company, 
+        desktop applications, website development, mobile app development,reservation 
+        systems developers, Tour booking systems, Search Engine optimasation, South African Software Companies,
+         kwaZulu Natal Software companies, South African IT companies, South African Systems Developers, Best IT companies, Eastern Cape Best Software Development companies" />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
