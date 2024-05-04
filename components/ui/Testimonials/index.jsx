73233b6162ea5042ef75cecdcbb8f0e9585db432
zwelisha <zwescientist@gmail.com>
2024-05-04 23:26:55 +0200

    Added Theo and Mthuthu's testimonial, and removed other testimonials

--- components/ui/Testimonials/index.jsx
+++ components/ui/Testimonials/index.jsx
@@ -4,11 +4,19 @@ const Testimonials = () => {
   const testimonials = [
     {
       avatar:
-        'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-      name: 'Andre De Kock',
-      title: 'Founder of Lyconf',
+        '/testimonials/mthuthu.png',
+      name: 'Mthuthu Ndebele',
+      title: 'Singer & Songwriter',
       quote:
-        'As a small business owner, I was doing everything and my workload was increasing. With this startup, I was able to save time so I could focus on the things that matter most: my clients and my family.',
+        'As a musician, digital branding and accessibility have become a necessity and Namoota has been nothing but the cut-edge provider making the navigation to my website friendly as it is nicely linked to my music and social media accounts making it easier to update it from time to time',
+    },
+    {
+      avatar:
+        '/testimonials/theo.png',
+      name: 'Theo Gina',
+      title: 'Founder of Theo Tours',
+      quote:
+        'My name is Xolani Theo Gina from Theo Tours. I would like to thank Zweli and his team for developing my company website. It helped me reach a lot of customers. Now my customers are able to book tours from my website.',
     },
     {
       avatar:
@@ -17,37 +25,6 @@ const Testimonials = () => {
       title: 'DevOps engineer',
       quote:
         "My company's software now is easy to use, saves time and money, and is loved by a lot of users. One customer saved R10k over the course of 3 years and another saves 8 hours per week! Thanks to Namoota.",
-    },
-    {
-      avatar:
-        'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
-      name: 'Simone Naidoo',
-      title: 'System manger',
-      quote:
-        'My business was in a dire situation. I had no idea what to do, and I felt like I was losing hope. Then I found this Startup and everything changed. It helped me create automated sales.',
-    },
-    {
-      avatar: 'https://randomuser.me/api/portraits/women/79.jpg',
-      name: 'Vanessa Ackermans',
-      title: 'Product designer',
-      quote:
-        'One day, my company was about to go under and I had no idea what to do. I found Namoota and it helped me get my business back on track.Now, my company is flourishing and I see new opportunities.',
-    },
-    {
-      avatar:
-        'https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80',
-      name: 'Jurica koletic',
-      title: 'Founder of Let’s code',
-      quote:
-        'In these difficult economic times, doing business is tough. Funding is hard to come by and many entrepreneurs are struggling to keep their doors open. but when I found this startup everything changed.',
-    },
-    {
-      avatar:
-        'https://images.unsplash.com/photo-1590038767624-dac5740a997b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-      name: 'Lonwabo Sithole',
-      title: 'Full stack engineer',
-      quote:
-        'We all know how costly it is to find good help. I was faced with this problem when I lost my data entry staff and my business was on the brink of going under. Thankfully, Namoota saved the day.',
     },
   ];
 
