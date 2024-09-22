eb79c7faecb968c57e17478dff90021dca300290
Matthew Fourie <mattfourie3@gmail.com>
2024-09-22 18:08:46 +0200

    Added styles to he team section

--- pages/team.js
+++ pages/team.js
@@ -4,11 +4,11 @@ import LogoGrid from '../components/ui/LogoGrid'
 import FooterCTA from '../components/ui/FooterCTA'
 import Head from 'next/head'
 import GradientWrapper from '../components/GradientWrapper'
-import aya from '/team/aya.jpeg'
-// import zweli from '../../public/team/zweliMthethwa.jpeg'
-// import kaatnanret from '../../public/team/dyelpoKaatNanretSenshak.jpeg'
-// import siya from '../../public/team/siyamthandaNomgqokwana.jpeg'
-// import matthew from '../../public/team/matthewFourie.jpeg'
+// import aya from '../public/team/aya.jpeg'
+// import zweli from '../public/team/zweliMthethwa.jpeg'
+// import kaatnanret from '../public/team/dyelpoKaatNanretSenshak.jpeg'
+// import siya from '../public/team/siyamthandaNomgqokwana.jpeg'
+// import matthew from '../public/team/matthewFourie.jpeg'
 
 export default function team() {
   const data = [
@@ -16,36 +16,41 @@ export default function team() {
       id: 1,
       name: 'Zweli Mthethwa',
       title: 'Co-founder/ Software Engineer',
-      imageUrl: zweli,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquet nunc, nec aliquam nunc nisl sit amet nunc. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquet nunc, nec aliquam nunc nisl sit amet nunc.'
+      imageUrl: "/team/zweli-removebg.png",
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel lacinia, nec aliquam nunc nisl sit amet nunc.',
+      color: '#782a2b'
     },
     {
       id: 2,
       name: 'Ayabonga Qwabi',
       title: 'Co-founder/ Software Engineer',
-      imageUrl: aya,
-      description: 'Aya develops efficient and scalable software solutions, contributing to the innovation and functionality of our products.'
+      imageUrl: "/team/aya-removebg.png",
+      description: 'Aya develops efficient and scalable software solutions, contributing to the innovation and functionality of our products.',
+      color: '#782a2b'
     },
     {
       id: 3,
       name: 'Siyamthanda Nomgqokwana',
       title: 'IT Infrastructure / Cloud Architect',
-      imageUrl: siya,
-      description: 'Siya architects and implements robust cloud solutions, shaping the technological foundation of our projects.'
+      imageUrl: "team/siya-removebg.png",
+      description: 'Siya architects and implements robust cloud solutions, shaping the technological foundation of our projects.',
+      color: '#37375e'
     },
     {
       id: 4,
       name: 'Matthew Fourie',
       title: 'Software Engineer',
-      imageUrl: matthew,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquet nunc, nec aliquam nunc nisl sit amet nunc. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquet nunc, nec aliquam nunc nisl sit amet nunc.'
+      imageUrl: "team/matt-removebg.png",
+      description: 'Lorem ipsum  amet nunc. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquet nunc, nisl sit amet nunc.',
+      color: '#3e6e7c'
     },
     {
       id: 5,
       name: 'Dyelpo KaatNanret Senshak',
       title: 'Social Media Specialist',
-      imageUrl: kaatnanret,
-      description: 'KaatNanret manages social media pages and creates content to reach target audiences and drive conversions.'
+      imageUrl: "team/kaatnanret-removebg.png",
+      description: 'KaatNanret manages social media pages and creates content to reach target audiences and drive conversions.',
+      color: '#3e6e7c'
     },
   ]
 
@@ -57,18 +62,23 @@ export default function team() {
       <Hero />
       <LogoGrid />
       <GradientWrapper>
+      <div className="flex flex-wrap justify-center gap-6 p-10">
         {data.map((member) => {
           return (
-            <div key={member.id} className='flex flex-col items-center justify-center'>
+            <div key={member.id} className='w-full md:w-[80%] xl:w-1/3 p-8 bg-white rounded-lg shadow-md transform hover:scale-105 transition-transform duration-300 ease-out'>
               <div className='flex flex-col items-center justify-center'>
-                <img src={`/public/${member.imageUrl}`} alt={member.name} className='w-48 h-48 rounded-full object-cover' />
-                <h2 className='text-2xl font-bold mt-4'>{member.name}</h2>
-                <p className='text-gray-600 text-lg mt-2'>{member.title}</p>
-                <p className='text-gray-600 text-lg mt-2'>{member.description}</p>
+                <img src={member.imageUrl} alt={member.name} width={120} height={120} className='w-32 h-32 rounded-full object-cover border-4 border-white shadow-lg'/>
+                <h2 className='text-xl text-gray-800 font-extrabold mx-auto sm:text-2xl'>{member.name}</h2>
+                <p className='text-lg font-semibold text-gray-700 sm:text-base'>{member.title}</p>
+                {/* divider line */}
+                <hr className="w-[70%] my-2 border-t border-gray-300" />
+                <p className='max-w-xl mx-auto text-gray-600 text-center'>{member.description}</p>
               </div>
             </div>
           )
         })}
+      </div>
+
       </GradientWrapper>
       <FooterCTA />
     </>
