b45b3b20c848cdbf83e2e69b81c49325d82c5f39
Ayabonga Qwabi <aya@simply.co.za>
2024-10-13 20:34:09 +0200

    Refactor Navbar component

--- components/ui/Navbar/index.jsx
+++ components/ui/Navbar/index.jsx
@@ -1,90 +1,96 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import Brand from '../Brand'
-import NavLink from '../NavLink'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Brand from '../Brand';
+import NavLink from '../NavLink';
 
 const Navbar = () => {
+  const [state, setState] = useState(false);
+  const { events } = useRouter();
 
-    const [state, setState] = useState(false)
-    const { events } = useRouter();
+  const navigation = [
+    { title: 'Features', path: '/#features' },
+    { title: 'Our toolkit', path: '/#toolkit' },
+    { title: 'Testimonials', path: '/#testimonials' },
+    { title: 'Team', path: '/team' },
+  ];
 
-    const navigation = [
-        { title: "Features", path: "/#features" },
-        { title: "Our toolkit", path: "/#toolkit" },
-        { title: "Testimonials", path: "/#testimonials" },
-        { title: "Team", path: "/team"}
-    ]
+  useEffect(() => {
+    // Close the navbar menu when navigate
+    const handleState = () => {
+      document.body.classList.remove('overflow-hidden');
+      setState(false);
+    };
+    events.on('routeChangeStart', () => handleState());
+    events.on('hashChangeStart', () => handleState());
+  }, []);
 
-    useEffect(() => {
-        // Close the navbar menu when navigate
-        const handleState = () => {
-            document.body.classList.remove("overflow-hidden")
-            setState(false)
-        }
-        events.on("routeChangeStart", () => handleState());
-        events.on("hashChangeStart", () => handleState());
-    }, [])
+  const handleNavMenu = () => {
+    setState(!state);
+    document.body.classList.toggle('overflow-hidden');
+  };
 
-    const handleNavMenu = () => {
-        setState(!state)
-        document.body.classList.toggle("overflow-hidden")
-    }
+  return (
+    <header class='flex items-center justify-between py-4  md:py-8'>
+      <a
+        href='/'
+        class='inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl'
+        aria-label='logo'
+      >
+        <img className='w-54 h-10' src='/logz.png' />
+      </a>
 
-    return (
-        <header>
-            <nav className={`bg-white w-full md:static md:text-sm ${state ? "fixed z-10 h-full" : ""}`}>
-                <div className="custom-screen items-center mx-auto md:flex">
-                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                        <Brand />
-                        <div className="md:hidden">
-                            <button role="button" aria-label="Open the menu" className="text-gray-500 hover:text-gray-800"
-                                onClick={handleNavMenu}
-                            >
-                                {
-                                    state ? (
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                                        </svg>
-                                    ) : (
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                                        </svg>
-                                    )
-                                }
-                            </button>
-                        </div>
-                    </div>
-                    <div className={`flex-1 pb-3 mt-8 md:pb-0 md:mt-0 md:block ${state ? "" : "hidden"}`}>
-                        <ul className="text-gray-700 justify-end items-center space-y-6 md:flex md:space-x-6 md:space-y-0 md:text-gray-600 md:font-medium">
-                            {
-                                navigation.map((item, idx) => {
-                                    return (
-                                        <li key={idx} className="duration-150 hover:text-gray-900">
-                                            <Link
-                                                href={item.path}
-                                                className="block"
-                                            >
-                                                {item.title}
-                                            </Link>
-                                        </li>
-                                    )
-                                })
-                            }
-                            <li>
-                                <NavLink
-                                    href="/get-started"
-                                    className="block font-medium text-sm text-white bg-gray-800 hover:bg-gray-600 active:bg-gray-900 md:inline"
-                                >
-                                    Let's work
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    )
-}
+      <nav class='hidden gap-12 lg:flex'>
+        <a href='#' class='text-lg font-semibold text-sky-500'>
+          Home
+        </a>
+        <a
+          href='#'
+          class='text-lg font-semibold text-gray-600 transition duration-100 hover:text-sky-500 active:text-sky-700'
+        >
+          Features
+        </a>
+        <a
+          href='#'
+          class='text-lg font-semibold text-gray-600 transition duration-100 hover:text-sky-500 active:text-sky-700'
+        >
+          Pricing
+        </a>
+        <a
+          href='#'
+          class='text-lg font-semibold text-gray-600 transition duration-100 hover:text-sky-500 active:text-sky-700'
+        >
+          About
+        </a>
+      </nav>
 
-export default Navbar
+      <a
+        href='#'
+        class='hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-sky-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block'
+      >
+        Contact Sales
+      </a>
+
+      <button
+        type='button'
+        class='inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-sky-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden'
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          class='h-6 w-6'
+          viewBox='0 0 20 20'
+          fill='currentColor'
+        >
+          <path
+            fill-rule='evenodd'
+            d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
+            clip-rule='evenodd'
+          />
+        </svg>
+        Menu
+      </button>
+    </header>
+  );
+};
+
+export default Navbar;
