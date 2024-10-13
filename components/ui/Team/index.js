13d41a86259b16cad50391c002b7625b6345559e
Ayabonga Qwabi <aya@simply.co.za>
2024-10-13 20:34:37 +0200

    Add Team and Testimonials components

---
+++ components/ui/Team/index.js
@@ -0,0 +1,93 @@
+const data = [
+  {
+    id: 1,
+    name: 'Zweli Mthethwa',
+    title: 'Co-founder/ Software Engineer',
+    imageUrl: '/team/zweli-removebg.png',
+    description:
+      'Zweli designs high-performance solutions and mentors the team, driving the technical vision of our projects.',
+    color: '#219ebc',
+  },
+  {
+    id: 2,
+    name: 'Ayabonga Qwabi',
+    title: 'Co-founder/ Software Engineer',
+    imageUrl: '/team/aya-removebg.png',
+    description:
+      'Aya develops efficient and scalable software solutions, contributing to the innovation and functionality of our products.',
+    color: '#219ebc',
+  },
+  {
+    id: 3,
+    name: 'Siyamthanda Nomgqokwana',
+    title: 'IT Infrastructure / Cloud Architect',
+    imageUrl: 'team/siya-removebg.png',
+    description:
+      'Siya architects and implements robust cloud solutions, shaping the technological foundations of our projects.',
+    color: '#219ebc',
+  },
+  {
+    id: 4,
+    name: 'Matthew Fourie',
+    title: 'Software Engineer',
+    imageUrl: 'team/matt-removebg.png',
+    description:
+      'Matthew supports the development team by writing clean and maintainable code, assisting in the creation of innovative software solutions that drive project success.',
+    color: '#219ebc',
+  },
+  {
+    id: 5,
+    name: 'Dyelpo KaatNanret Senshak',
+    title: 'Social Media Specialist',
+    imageUrl: 'team/kaatnanret-removebg.png',
+    description:
+      'KaatNanret manages social media pages and creates content to reach target audiences and drive conversions.',
+    color: 'black',
+  },
+];
+
+const Team = () => (
+  <section class='bg-white dark:bg-gray-900'>
+    <div class='container px-6 py-10 mx-auto'>
+      <h1 class='mb-8 text-3xl font-bold text-black sm:text-4xl md:mb-12 md:text-5xl'>
+        Meet <span class='text-sky-500'>Our Team</span>
+      </h1>
+
+      <p class='max-w-4xl my-6 text-sky-800 dark:text-gray-300 text-2xl text-left'>
+        We empower businesses by crafting bespoke software solutions that
+        transcend expectations. Namoota crafts innovative applications that
+        integrate perfectly, driving exceptional customer experiences.
+      </p>
+
+      <div class='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-2'>
+        {data.map((p) => (
+          <div class='px-12 py-8 transition-colors duration-300 transform border cursor-pointer rounded-xl hover:border-transparent group hover:bg-sky-600 dark:border-gray-700 dark:hover:border-transparent'>
+            <div class='flex flex-col sm:-mx-4 sm:flex-row'>
+              <img
+                class='flex-shrink-0 object-cover w-24 h-24 rounded-full sm:mx-4 ring-4 ring-gray-300'
+                src={p.imageUrl}
+                alt=''
+              />
+
+              <div class='mt-4 sm:mx-4 sm:mt-0'>
+                <h1 class='text-xl font-semibold text-gray-700 capitalize md:text-2xl dark:text-white group-hover:text-white'>
+                  {p.name}
+                </h1>
+
+                <p class='mt-2 text-gray-500 capitalize dark:text-gray-300 group-hover:text-gray-300'>
+                  {p.title}
+                </p>
+              </div>
+            </div>
+
+            <p class='mt-4 text-gray-500 capitalize dark:text-gray-300 group-hover:text-gray-300'>
+              {p.description}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>
+);
+
+export default Team;
--- components/ui/Testimonials/index.jsx
+++ components/ui/Testimonials/index.jsx
@@ -3,18 +3,18 @@ import SectionWrapper from '../../SectionWrapper';
 const Testimonials = () => {
   const testimonials = [
     {
-      avatar:
-        '/testimonials/mthuthu.png',
+      avatar: '/testimonials/mthuthu.png',
       name: 'Mthuthu Ndebele',
       title: 'Singer & Songwriter',
+      heading: 'Great digital branding and accessibility!',
       quote:
         'As a musician, digital branding and accessibility have become a necessity and Namoota has been nothing but the cut-edge provider making the navigation to my website friendly as it is nicely linked to my music and social media accounts making it easier to update it from time to time',
     },
     {
-      avatar:
-        '/testimonials/theo.png',
+      avatar: '/testimonials/theo.png',
       name: 'Theo Gina',
       title: 'Founder of Theo Tours',
+      heading: 'Excellent Websites for Marketing!',
       quote:
         'My name is Xolani Theo Gina from Theo Tours. I would like to thank Zweli and his team for developing my company website. It helped me reach a lot of customers. Now my customers are able to book tours from my website.',
     },
@@ -23,52 +23,46 @@ const Testimonials = () => {
         'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
       name: 'Shaun Adams',
       title: 'DevOps engineer',
+      heading: 'Money saving solutions at hand',
       quote:
         "My company's software now is easy to use, saves time and money, and is loved by a lot of users. One customer saved R10k over the course of 3 years and another saves 8 hours per week! Thanks to Namoota.",
     },
   ];
 
   return (
-    <SectionWrapper className='pb-0'>
-      <div id='testimonials' className='max-w-screen-xl mx-auto px-4 md:px-8'>
-        <div className='max-w-2xl sm:text-center md:mx-auto'>
-          <h2 className='text-gray-800 text-3xl font-semibold sm:text-4xl'>
-            See what others are saying about us
-          </h2>
-          <p className='mt-3 text-gray-600'>
-            Listen to what the experts around the country are saying about us.
-          </p>
-        </div>
-        <div className='mt-12'>
-          <ul className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
-            {testimonials.map((item, idx) => (
-              <li key={idx} className='bg-white border p-4 rounded-xl'>
-                <figure>
-                  <div className='flex items-center gap-x-4'>
-                    <img
-                      src={item.avatar}
-                      className='w-14 h-14 object-cover rounded-full'
-                      alt={item.name}
-                    />
-                    <div>
-                      <span className='block text-gray-800 font-semibold'>
-                        {item.name}
-                      </span>
-                      <span className='block text-gray-600 text-sm mt-0.5'>
-                        {item.title}
-                      </span>
-                    </div>
+    <div class='bg-white py-6 sm:py-8 lg:py-12'>
+      <div class='mx-auto max-w-screen-xl px-4 md:px-8'>
+        <h2 class='mb-8 text-center text-4xl font-bold text-sky-800 md:mb-12 lg:text-5xl'>
+          What others say about us
+        </h2>
+
+        <div class='grid gap-y-10 sm:grid-cols-2 sm:gap-y-12 lg:grid-cols-3 lg:divide-x'>
+          {testimonials.map((t) => (
+            <div class='flex flex-col items-center justify-between gap-4 sm:px-4 md:gap-6 lg:px-8'>
+              <div class='text-center text-2xl text-gray-600'>“{t.quote}”</div>
+
+              <div class='flex flex-col items-between justify-between gap-2 sm:flex-row md:gap-3'>
+                <div class='h-12 w-12 overflow-hidden rounded-full bg-gray-100 shadow-lg md:h-14 md:w-14'>
+                  <img
+                    src={t.avatar}
+                    loading='lazy'
+                    alt={t.name}
+                    class='h-full w-full object-cover object-center'
+                  />
+                </div>
+
+                <div>
+                  <div class='text-center text-xl font-bold text-indigo-500 sm:text-left'>
+                    {t.name}
                   </div>
-                  <blockquote>
-                    <p className='mt-6 text-gray-700'>{item.quote}</p>
-                  </blockquote>
-                </figure>
-              </li>
-            ))}
-          </ul>
+                  <p class='text-center text-lg text-gray-500 '>{t.title}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    </SectionWrapper>
+    </div>
   );
 };
 
