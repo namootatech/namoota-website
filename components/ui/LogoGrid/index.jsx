b9fe30207b52a632eff4cbed16024c0c875a2509
Ayabonga Qwabi <aya@simply.co.za>
2024-10-13 20:33:55 +0200

    Refactor LogoGrid component to improve UI and add hover effect

--- components/ui/LogoGrid/index.jsx
+++ components/ui/LogoGrid/index.jsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import clinicplus from '../../../public/logos/1.png';
 import tecla from '../../../public/logos/2.webp';
 import siavista from '../../../public/logos/3.png';
-import adobe from '../../../public/logos/adobe.svg';
 import theotours from '../../../public/logos/theotours.png';
 import africancreative from '../../../public/logos/africancreativeartforms.png';
 import musicloopy from '../../../public/logos/musicloopy.png';
@@ -34,26 +33,37 @@ const logos = [
   },
 ];
 
-
 const LogoGrid = () => (
-  <div>
-    <div className='custom-screen'>
-      <h2 className='font-semibold text-sm text-gray-600 text-center'>
-        Exceptional experiences crafted for:
-      </h2>
-      <div className='mt-6'>
-        <ul className='flex gap-x-10 gap-y-6 flex-wrap items-center justify-center md:gap-x-16'>
-          {logos.map((item, idx) => (
-            <li key={idx}>
-              <Image
-                src={item.src}
-                alt={item.alt}
-                width={100}
-                height={100}
-              />
-            </li>
-          ))}
-        </ul>
+  <div className='bg-white py-6 sm:py-8 lg:py-12 mt-20'>
+    <div className='mx-auto max-w-screen-2xl px-4 md:px-2'>
+      <div className='mb-4 flex flex-col items-center md:mb-8 lg:flex-row lg:justify-between'>
+        <h2 className='mb-2 text-center text-2xl font-bold text-sky-800 lg:mb-0 lg:text-3xl'>
+          Trusted by Forward-Thinking Businesses
+        </h2>
+
+        <p className='max-w-md text-center text-sm text-sky-600 lg:text-right'>
+          From digital transformation to bespoke software solutions, these
+          businesses trust us to drive their success in the digital world.
+        </p>
+      </div>
+
+      <div className='grid grid-cols-2 gap-4 rounded-lg md:grid-cols-3 lg:gap-10'>
+        {logos.map((item, idx) => (
+          <div
+            key={idx}
+            className='m-8 relative shadow-md flex h-16 items-center justify-center rounded-2xl bg-sky-50 p-4 text-gray-400 sm:h-32 border-r-8 border-r-sky-700 border-r-solid'
+          >
+            {/* Image component */}
+            <Image
+              className='h-6 w-auto sm:h-16'
+              src={item.src}
+              alt={item.alt}
+            />
+
+            {/* sky overlay with hover effect */}
+            <div className='absolute inset-0 bg-sky-600 opacity-50 hover:opacity-0 transition-opacity duration-300 rounded-xl'></div>
+          </div>
+        ))}
       </div>
     </div>
   </div>
