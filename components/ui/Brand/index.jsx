75b02656ab69678054aefe1c3acadc3cb661570c
Ayabonga Qwabi <aya@simply.co.za>
2024-10-13 20:33:33 +0200

    Update Brand component with new logo

--- components/ui/Brand/index.jsx
+++ components/ui/Brand/index.jsx
@@ -2,7 +2,11 @@ import Link from 'next/link';
 
 const Brand = () => (
   <Link href='/'>
-    <img src='/logo2.png' width={160} height={80} alt='Namoota logo' />
+    <img
+      src='/logo-id.png'
+      className='w-720 h-16 border-4 border-solid border-sky-700 rounded-md'
+      alt='Namoota logo'
+    />
   </Link>
 );
 export default Brand;
