d399eb6fe39b7fdcc6d00041505931423066ee04
Ayabonga Qwabi <aya@simply.co.za>
2024-11-20 16:09:38 +0200

    secretise firebase config

--- util/firebase.js
+++ util/firebase.js
@@ -13,13 +13,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBM70qNKTNSCYEJvf8_YUQVbLEe7PDeAOU',
-  authDomain: 'namoota-bb3f9.firebaseapp.com',
-  projectId: 'namoota-bb3f9',
-  storageBucket: 'namoota-bb3f9.firebasestorage.app',
-  messagingSenderId: '593991572759',
-  appId: '1:593991572759:web:9ea9b60a4d4030551c6b52',
-  measurementId: 'G-MBRKVPZ1R6',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
